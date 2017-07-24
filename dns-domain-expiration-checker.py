46e8f08417411651b50310c3c5ff83df21ae6248
Matty <Matty9191@users.noreply.github.com>
2017-07-24 12:11:44 -0400

    Update dns-domain-expiration-checker.py

--- dns-domain-expiration-checker.py
+++ dns-domain-expiration-checker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Program: DNS Domain Expiration Checker
 # Author: Matty < matty91 at gmail dot com >
-# Current Version: 1.0
+# Current Version: 1.1
 # Date: 07-20-2017
 # License:
 #  This program is free software; you can redistribute it and/or modify
@@ -14,6 +14,7 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU General Public License for more details.
 
+import sys
 import time
 import whois
 import argparse
@@ -23,9 +24,20 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
 DEBUG = 0
-SMTP_TO = ""
-SMTP_FROM = ""
-SMTP_SERVER = ""
+
+
+def print_heading():
+    """
+       Print a formatted heading when called interactively
+    """
+    print("%-25s  %-30s  %-20s  %-4s" % ("Domain Name", "Registrar", "Expiration Date", "Days Left"))
+
+
+def print_domain(domain, registrar, expiration_date, days_remaining):
+    """
+       Pretty print the domain information on stdout
+    """
+    print("%-25s  %-30s  %-20s  %-4s" % (domain, registrar, expiration_date, days_remaining))
 
 
 def debug(string_to_print):
@@ -36,13 +48,25 @@ def debug(string_to_print):
         print(string_to_print)
 
 
-def calculate_dns_expiration(domain, expire_days):
+def send_whois_query(domain):
+    """
+       Issue a WHOIS query for the domain passed as an argument. return
+       the expiration date and registrar
+    """
+    try:
+        query_result = whois.query(domain)
+    except:
+        print("Unable to retrieve WHOIS data for domain %s" % domain)
+        sys.exit(1)
+
+    return query_result.expiration_date, query_result.registrar
+
+
+def calculate_dns_expiration(domain, expire_days, expiration_date):
     """
        Check to see when a domain will expire
     """
-
-    query_result = whois.query(domain)
-    domain_expire = query_result.expiration_date - datetime.now()
+    domain_expire = expiration_date - datetime.now()
 
     debug("Domain expire days %s, Expire warning days %s" % (domain_expire.days, expire_days))
 
@@ -58,35 +82,40 @@ def check_expired(domainname, expiration_days, days_remaining):
        day threshold. If so send out notifications
     """
     if int(days_remaining) < int(expiration_days):
-        domain_expire_notify(domainname, days_remaining)
+        return days_remaining
+    else:
+        return 0
 
 
-def domain_expire_notify(domain, days):
+def domain_expire_notify(domain, days, config_options):
     """
        Functions to call when a domain is about to expire. Adding support
        for Nagios, SNMP, etc. can be done by defining a new function and
        calling it here.
     """
     debug("Triggering notifications for the DNS domain %s" % domain)
-    send_expire_email(domain, days)
+
+    # Send outbound e-mail if a rcpt is passed in
+    if config_options["email"]:
+        send_expire_email(domain, days, config_options)
 
 
-def send_expire_email(domain, days):
+def send_expire_email(domain, days, config_options):
     """
        Generate an e-mail to let someone know a domain is about to expire
     """
-    debug("Generating an e-mail to %s for domain %s" % (SMTP_TO, domain))
+    debug("Generating an e-mail to %s for domain %s" % (config_options["smtpto"], domain))
     msg = MIMEMultipart()
-    msg['From'] = SMTP_FROM
-    msg['To'] = SMTP_TO
+    msg['From'] = config_options["smtpfrom"]
+    msg['To'] = config_options["smtpto"]
     msg['Subject'] = "The DNS Domain %s is set to expire in %d days" % (domain, days)
 
     body = "Time to renew %s" % domain
     msg.attach(MIMEText(body, 'plain'))
 
-    smtp_connection = smtplib.SMTP(SMTP_SERVER)
+    smtp_connection = smtplib.SMTP(config_options["smtpserver"],config_options["smtpport"])
     message = msg.as_string()
-    smtp_connection.sendmail(SMTP_FROM, SMTP_TO, message)
+    smtp_connection.sendmail(config_options["smtpfrom"], config_options["smtpto"], message)
     smtp_connection.quit()
 
 
@@ -95,56 +124,64 @@ def processcli():
         parses the CLI arguments and returns a domain or 
         a file with a list of domains.
     """
-    global SMTP_TO
-    global SMTP_FROM
-    global SMTP_SERVER
-    global SMTP_PORT
-    global DEBUG
-
     parser = argparse.ArgumentParser(description='DNS Statistics Processor')
 
     parser.add_argument('--domainfile', help="Path to file with list of domains and expiration intervals.")
     parser.add_argument('--domainname', help="Domain to check expiration on.")
-    parser.add_argument('--debug',action="store_true", help="Enable debugging output.")
+    parser.add_argument('--email', action="store_true", help="Enable debugging output.")
+    parser.add_argument('--interactive',action="store_true", help="Enable debugging output.")
     parser.add_argument('--expiredays', default=90, help="Expiration threshold to check against.")
     parser.add_argument('--sleeptime', default=5, help="Time to sleep between whois queries.")
     parser.add_argument('--smtpserver', default="localhost", help="SMTP server to use.")
     parser.add_argument('--smtpport', default=25, help="SMTP port to connect to.")
     parser.add_argument('--smtpto', default="root", help="SMTP To: address.")
     parser.add_argument('--smtpfrom', default="root", help="SMTP From: address.")
-    args = parser.parse_args()
 
-    # TODO: Need to research ways to hold the SMTP configs w/o globals.
-    SMTP_TO = args.smtpto
-    SMTP_FROM = args.smtpfrom
-    SMTP_PORT = args.smtpport
-    SMTP_SERVER = args.smtpserver
-    DEBUG = args.debug
-
-    return args.domainfile, args.domainname, args.expiredays, args.sleeptime
-
+    return(vars(parser.parse_args()))
+           
 
 def main():
    """
        Main loop
    """
    days_remaining = 0
-   domainfile, domainname, expire_days, sleeptime =  processcli()
+   conf_options = processcli()
 
-   if domainfile:
-       with open(domainfile, "r") as domains_to_process:
+   if conf_options["interactive"]:
+       print_heading()
+   
+   if conf_options["domainfile"]:
+       with open(conf_options["domainfile"], "r") as domains_to_process:
            for line in domains_to_process:
                 domainname, expiration_days = line.split()
                 debug("Checking domain %s" % domainname)
-                days_remaining = calculate_dns_expiration(domainname, expiration_days)
-                check_expired(domainname, expiration_days, days_remaining)
-                time.sleep(sleeptime)
+                expiration_date, registrar = send_whois_query(domainname)
+                days_remaining = calculate_dns_expiration(domainname, expiration_days, expiration_date)
+
+                if check_expired(domainname, expiration_days, days_remaining):
+                    domain_expire_notify(domainname, days_remaining, conf_options)
+
+                if conf_options["interactive"]:
+                    print_domain(domainname, registrar, expiration_date, days_remaining)
+
+                # Need to wait between queries to avoid triggering DOS measures like this:
+                # Your IP has been restricted due to excessive access, please wait a bit
+                time.sleep(conf_options["sleeptime"])
    elif domainname:
        debug("Checking domain %s" % domainname)
+       expiration_date, registrar = send_whois_query(domainname)
        days_remaining = calculate_dns_expiration(domainname, expire_days)
-       check_expired(domainname, expire_days, days_remaining)
-       time.sleep(sleeptime)
+       if check_expired(domainname, expire_days, days_remaining):
+           domain_expire_notify(domainname, days_remaining, conf_options)
+
+       if conf_options["interactive"]:
+           print_domain(domain, registrar, expiration_date, days_remaining)
+
+       # Need to wait between queries to avoid triggering DOS measures like this:
+       # Your IP has been restricted due to excessive access, please wait a bit
+       time.sleep(conf_options["sleeptime"])
 
 
 if __name__ == "__main__":
     main()
+(domain-check) 
