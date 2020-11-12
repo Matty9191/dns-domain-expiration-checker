726fbd10352fc2dd99677e113add27065e47bc1d
Matty <Matty9191@users.noreply.github.com>
2020-11-12 11:47:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name = "dns-domain-expiration-checker",
     version = "6.0",
     author = "Matty",
-    author_email = "matty91@gmail.com",
+    author_email = "matty @ prefetch dot net",
     url = 'https://github.com/Matty9191/dns-domain-expiration-checker',
-    description = "DNS DOmain Expiration Checker",
+    description = "DNS Domain Expiration Checker",
     keywords = "DNS Domain Expiration Bind",
     packages = find_packages(),
     install_requires = ['python-dateutil'],
 )
+ 
